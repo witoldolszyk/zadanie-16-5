5e4635402f2543c7cdd8de56e2a7c961e90ef96e
Witold Olszyk <witoldolszyk@gmail.com>
2018-03-19 00:56:01 +0100

    add all new appplication with todo

---
+++ src/containers/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+import styleII from './TodoList.css';
+import Title from '../components/Title.js'
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+              {
+                id: 1,
+                    text: 'clean room'
+                }, {
+                id: 2,
+                    text: 'wash the dishes'
+                }, {
+                id: 3,
+                    text: 'feed my cat'
+                }
+            ]
+        };
+    }
+    addTodo(val){
+        const todo = {
+            text: velo,
+            id: uuid.v4()
+          }
+        const data = [...this.state.data, todo];
+        this.setState({data});
+
+        }
+    removeTodo(id) {
+      const remainder = this.state.data.filter(todo => todo.id !== id);
+      this.setState({data: remainder});
+    }
+
+
+    render() {
+      return (
+          <div className={style.TodoApp}>
+
+            <Title title={'Do zrobienia'}>
+            </Title>
+            <div className={style2.TodoList}>
+              <TodoList todos={this.state.data} remove={this.removeTodo}>
+              </TodoList>
+            </div>
+          </div>
+      );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+
+module.exports = {
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+  }),
+  new webpack.optimize.UglifyJsPlugin(),
+  new OptimizeJsPlugin({
+  sourceMap: false
+})
+
+],
+
+    entry: './src/index.js',
+        output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                include: /css/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
