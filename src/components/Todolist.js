517b44f26a7df2d68018cbb6710f73b7d09e605e
Witold Olszyk <witoldolszyk@gmail.com>
2018-03-19 23:40:14 +0100

    add news

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -9,15 +9,4 @@ const TodoList = ({todos, remove}) => {
   }
 
 
-export default TodoList;import React from 'react';
-import Todo from './Todo.js';
-
-const TodoList = ({todos, remove}) => {
-    const list = todos.map((todo) => {
-        return (<Todo todo={todo} key={todo.id} remove={remove}/>);
-    });
-    return (<ol start='1' type='1'>{list}</ol>);
-  }
-
-
 export default TodoList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
-import styleII from './TodoList.css';
-import Title from '../components/Title.js'
+import drugistyl from './TodoList.css';
+import Title from '../components/Title.js';
+import TodoList from '../components/TodoList.js';
 
 class App extends React.Component {
     constructor(props){
@@ -43,7 +44,7 @@ class App extends React.Component {
 
             <Title title={'Do zrobienia'}>
             </Title>
-            <div className={style2.TodoList}>
+            <div className={drugistyl.TodoList}>
               <TodoList todos={this.state.data} remove={this.removeTodo}>
               </TodoList>
             </div>
